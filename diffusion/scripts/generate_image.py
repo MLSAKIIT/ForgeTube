Fixed Guidance Scaling to 9

diffusion/scripts/generate_image.py
@@ -14,14 +14,17 @@ image = modal.Image.debian_slim().pip_install(
 app = modal.App(name="finalgen_app")
 
 @app.function(image=image, gpu="A10G")
-def generate_image(prompt, negative_prompt="", steps=50, guidance_scale=9, width=1980, height=1080, seed=None):
+def generate_image(prompt, negative_prompt="", steps=50, guidance_scale=9, width=1920, height=1080, seed=None):
     import torch
-    from diffusers import StableDiffusionPipeline
+    from diffusers import DiffusionPipeline
 
 
 # LOADS THE DIFFUSION PIPELINE
 
-    pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+    pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", 
+                                            torch_dtype=torch.float16,
+                                            use_safetensors=True, 
+                                            variant="fp16")
     pipe.to("cuda")
 
     generator = torch.Generator(device="cuda").manual_seed(seed) if seed else None
@@ -76,7 +79,9 @@ def main_generate_image(script_path,images_output_path):
                 timestamp = scene.get("timestamp", f"{idx:03d}")
                 negative_prompt = scene.get("negative_prompt", "")
                 steps = scene.get("steps", 50)
-                guidance_scale = scene.get("guidance_scale", 12)
+                # guidance_scale = scene.get("guidance_scale", 12)
+                guidance_scale = 9
+
                 # width = scene.get("width", 1024)
                 width = 1920
                 # height = scene.get("height", 576)